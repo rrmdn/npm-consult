7ac6a5084fd115a4ee42653caa48bca4ac66474a
Rizki Romadhoni <rromadhoni@users.noreply.github.com>
2017-11-05 07:07:47 +0700

    [feature] print unsupported packages (#8)

--- src/lib/DependencyGraph.js
+++ src/lib/DependencyGraph.js
@@ -4,6 +4,23 @@ import semver from 'semver';
 import Package from './Package';
 import PackageVersions from './PackageVersions';
 
+class Result<Ok, Err> {
+  static err = (err: Err): Result<Ok, Err> =>
+    new Result(null, err);
+  static ok = (ok: Ok): Result<Ok, Err> =>
+    new Result(ok, null)
+  constructor(ok: Ok, err: Err) {
+    this.ok = ok;
+    this.err = err;
+  }
+  match = (ifOk: (ok: Ok) => any, ifErr: (err: Err) => any): Ok => {
+    if (this.err) {
+      return ifErr(this.err);
+    }
+    return ifOk(this.ok);
+  }
+}
+
 export default class DependencyGraph {
   dependencyMap: Map<string, string>;
   dependencies: Array<PackageVersions>;
@@ -11,17 +28,15 @@ export default class DependencyGraph {
     this.dependencyMap = dependencyMap;
   }
   async resolveDependencyGraph() {
-    this.dependencies = Array.from(this.dependencyMap.keys()).map(
-      packageName => new PackageVersions(packageName)
-    );
-    await Promise.all(
-      this.dependencies.map(pkgVersion => pkgVersion.resolveVersions())
-    );
+    this.dependencies = Array
+      .from(this.dependencyMap.keys())
+      .map(packageName => new PackageVersions(packageName));
+    await Promise.all(this.dependencies.map(pkgVersion => pkgVersion.resolveVersions()));
     return true;
   }
   removePackagesWithout(packageToUpdate: string) {
     const newDependencies = [];
-    this.dependencies.forEach(pkgVersion => {
+    this.dependencies.forEach((pkgVersion) => {
       if (pkgVersion.dependsOn(packageToUpdate)) {
         newDependencies.push(pkgVersion);
       } else {
@@ -30,28 +45,36 @@ export default class DependencyGraph {
     });
     this.dependencies = newDependencies;
   }
-  packagesToUpdate(packageToUpdate: string, version: string): Array<Package> {
-    const packagesToUpdate = [];
-    this.dependencies.forEach(pkgVersions => {
-      let foundPackageVersion;
-      pkgVersions.versions.forEach(pkg => {
-        const packageToLookFor = pkg.dependencies.find(
-          dep => dep.name === packageToUpdate
-        );
+  packagesToUpdate = (
+    packageToUpdate: string,
+    version: string,
+  ): Result<Array<Package>, Array<Package>> => {
+    const packagesToUpdate: Array<Package> = [];
+    const unsupportedPackages: Array<Package> = [];
+    this.dependencies.forEach((pkgVersions) => {
+      let foundPackageVersion: Package;
+      pkgVersions.versions.forEach((pkg) => {
+        const packageToLookFor = pkg.dependencies.find(dep => dep.name === packageToUpdate);
         if (packageToLookFor) {
           const acceptedRange = semver.validRange(packageToLookFor.version);
           if (acceptedRange) {
             const isOnTheRange = semver.satisfies(version, acceptedRange);
             if (isOnTheRange) {
-              foundPackageVersion = pkg.toString();
+              foundPackageVersion = pkg.copy();
             }
           }
         }
       });
       if (foundPackageVersion) {
         packagesToUpdate.push(foundPackageVersion);
+      } else {
+        unsupportedPackages
+          .push(pkgVersions.versions[pkgVersions.versions.length - 1].copy());
       }
     });
-    return packagesToUpdate;
+    if (unsupportedPackages.length) {
+      return Result.err(unsupportedPackages);
+    }
+    return Result.ok(packagesToUpdate);
   }
 }
